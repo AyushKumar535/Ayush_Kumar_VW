43ec202ab67cf545716eb0b2829fafb7017e8089
Ayush Kumar <ayushrana03dec2003@gmail.com>
2026-02-11 00:41:26 +1300

    Delete Assignment directory

--- Assignment/Day 2/q4.py
+++
@@ -1,6 +0,0 @@
-a = int(input("Enter first Number :"))
-b = int(input("Enter second Number :"))
-c = int(input("Enter third Number :"))
-
-average = (a+b+c) / 3
-print("Average =",average)
--- Assignment/Day 2/q7.py
+++
@@ -1,8 +0,0 @@
-num = float(input("Enter a numb :"))
-
-if num>0:
-    print("It's a Positive Numb")
-elif num < 0:
-    print("It's a Negative Numb")
-else:
-    print("Numb is ZERO")
