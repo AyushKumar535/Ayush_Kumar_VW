dc05a0ccee23ed716b7737f134105265e6a590bb
Ayush Kumar <ayushrana03dec2003@gmail.com>
2026-02-10 23:25:55 +1300

    Delete Assignment directory

--- Assignment/Day 2/q1.py
+++
@@ -1,9 +0,0 @@
-length = int(input("Enter a length :"))
-breadth = int(input("Enter a breadth :"))
-print(type(length))
-
-area = length * breadth
-perimeter = 2 * (length + breadth)
-
-print("Area of rectangle =",area)
-print("Perimeter of rectangle =",perimeter)
--- Assignment/Day 2/q6.py
+++
@@ -1,6 +0,0 @@
-age = int(input("Enter ur Age :"))
-
-if age>=18:
-    print("U r eligible for voting")
-else:
-    print("U r not eligible for voting")
